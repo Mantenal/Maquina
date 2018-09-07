d107836ed3eb0d02e55ab02f8a37ef52783775e8
Unknown <carlosloco41@gmail.com>
2018-09-07 18:57:01 -0500

    Interfaces

--- app/src/main/java/maquina/maquina/Login.java
+++ app/src/main/java/maquina/maquina/Login.java
@@ -8,8 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button Registro,Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,16 +19,13 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Login=findViewById(R.id.B_ingresar_L);
+        Registro=findViewById(R.id.B_registro_L);
 
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
+
 
 
 }
--- app/src/main/java/maquina/maquina/Registro.java
+++ app/src/main/java/maquina/maquina/Registro.java
@@ -1,22 +1,45 @@
 package maquina.maquina;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
 
+    Button Login,Registro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Login= findViewById(R.id.B_login);
+        Registro=findViewById(R.id.B_registrar);
 
+        Login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent log= new Intent(getApplicationContext(),Login.class);
+                startActivity(log);
+            }
+        });
+
+        Login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Registro Exitoso", Toast.LENGTH_SHORT);
+            }
+        });
 
     }
+
+
 
 }
